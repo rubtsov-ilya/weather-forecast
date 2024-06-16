225d3d4fb094496fcad6e3e46d72f81fa329702e
rubtsov-ilya <rubtsovilya398@gmail.com>
2024-06-16 23:39:34 +0300

    added html markup to home-page and custom select for search in database

--- src/components/pages/home-page/first-section/FirstSection.tsx
+++ src/components/pages/home-page/first-section/FirstSection.tsx
@@ -1,11 +1,19 @@
+/// <reference types='vite-plugin-svgr/client' />
+
 import styles from "./FirstSection.module.scss";
 import { FC } from "react";
+import LogoSvg from "../../../../assets/images/logo-icons/Logo-main-page.svg?react"
+import SearchSelect from "../../../ui/search-select/SearchSelect";
 
 const FirstSection: FC = () => {
   return (
     <section className={styles["first-section"]}>
       <div className="container">
         <div className={styles["first-section__content"]}>
+          <LogoSvg className={styles["first-section__logo"]}/>
+          <h1 className={styles["first-section__title"]}>Добро пожаловть в <span className={styles["first-section__title-span"]}>TypeWeather</span></h1>
+          <p className={styles["first-section__subtitle"]}>Выберите город, чтобы увидеть прогноз погоды</p>
+          <SearchSelect />
         </div>
       </div>
     </section>
---
+++ src/components/ui/search-select/SearchSelect.tsx
@@ -0,0 +1,45 @@
+import { FC, useLayoutEffect, useState } from 'react'
+import './SearchSelect.scss'
+import Select from "react-select";
+import { SingleValue } from 'react-select';
+import citiesDb from '../../../../data_base/cities-db.json'
+
+const SearchSelect: FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+  console.log(inputValue)
+
+  useLayoutEffect(() => {
+    if (inputValue.length > 0) {
+      setMenuIsOpen(true);
+    } else {
+      setMenuIsOpen(false);
+    }
+    
+  }, [inputValue])
+
+  const NoOptionsMessage = () => (
+    <div className="custom-select__no-options-message">
+      Ничего не найдено
+    </div>
+  );
+
+  return (
+    <Select
+      placeholder='Поиск города'
+      onInputChange={(inputValue: string) => setInputValue(inputValue)}
+      isSearchable={true}
+      className="custom-select"
+      classNamePrefix="custom-select"
+      name="color"
+      options={citiesDb}
+      menuIsOpen={menuIsOpen}
+      hideSelectedOptions={true}
+      components={{
+        NoOptionsMessage
+      }}
+    />
+  )
+}
+
+export default SearchSelect
